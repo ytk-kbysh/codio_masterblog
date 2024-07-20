655332b8505d2898f9677987cbf6c0c36b1ffdda
ytk-kbysh <kobayashi4registrations@utakata.jetzt>
2024-07-20 14:16:25 +0200

    add.html added

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    f = open('data.json')
-    blog_posts = json.load(f)
+    with open('database.json') as f:
+        blog_posts = json.load(f)
     return render_template('index.html', posts=blog_posts)
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=4999)
